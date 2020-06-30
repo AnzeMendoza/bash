b017eafe8c364405aa8b32fd9f3623d59fb7a321
Anze Mendoza <anze.mendoza@gmail.com>
2020-06-30 13:06:07 -0300

    se arreglo el problema 23 el cual el DU retorna varios valores y no permitia la comparación

--- script/23-continueBreak.sh
+++ script/23-continueBreak.sh
@@ -5,15 +5,17 @@
 
 # encuentra ficheros vacios en un directorio
 
-for fichero in $( ls /var/log ); do 
-    if [ -e /var/log/$fichero -a ! -s /var/log/$fichero ]; then
-        #echo El $fichero esta vacios
-        break # si solo quiero al primer elemento
-    fi
-done
+#for fichero in $( ls /var/log ); do 
+#    if [ -e /var/log/$fichero -a ! -s /var/log/$fichero ]; then
+#        #echo El $fichero esta vacios
+#        break # si solo quiero al primer elemento
+#    fi
+#done
+
+# para solucinar el problema de que du me devuelva 2 valores ya que eran directorios, le agregue --a
 
 for fichero in $( ls /var/log ); do
-    if [ $(du "/var/log/$fichero" | cut -f1) -eq 0 ]; then
+    if [ $(du --max-depth=0 "/var/log/$fichero" | cut -f1) -eq 0 ]; then
         echo El $fichero esta vacio 
     fi
 done
